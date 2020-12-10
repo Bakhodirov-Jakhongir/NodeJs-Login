a658a4b99adac6ca7b0adbdfa4925b263012c697
Bakhodirov-Jakhongir <64568158+Bakhodirov-Jakhongir@users.noreply.github.com>
2020-12-10 19:58:39 +0500

    First-Backend-Project
    This is my first Backend project.I use expressJs  , NodeJs , Mongoose to built it .

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.satrj.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+//mongodb+srv://Jakhongir:<password>@cluster0.satrj.mongodb.net/<dbname>?retryWrites=true&w=majority
